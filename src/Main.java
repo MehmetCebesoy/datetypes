bdd2f612af2343418b9e0b0145cea902034e2410
Mehmet CEBESOY <mhmtcbsy@gmail.com>
2022-02-06 00:36:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// int: integer
+        // byte < short < int < long
+        // double:ondalıklı sayıları tutumak için kullanılır 10.5 gibi
+        // float < double
+        // char : tek karakter tipi için kullanılır örn: char karakter = 'a';
+        // boolean: karşılığı doğru veya yanlıştır 0 veya 1 , sayısal bir değerdir.
+        // ascil kodu: her bir terimin sayısal karşılığıdır
+        int sayi =12;
+        sayi = "manisa";
+    }boolean dogruMu = true;
+}
